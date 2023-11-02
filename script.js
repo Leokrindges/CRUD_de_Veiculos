abac0d715fc6d4bfd40e6c80bc5092909ee41123
leonardo Krindges <krindgesleonardo3@gmail.com>
2023-11-02 16:53:18 -0300

    add: funcao remover e atualizar, corrigido bugs

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-
-document.write(`Bem-vindo ao sistema de CRUD de veículos:\n`)
-// document.write(`No momento, o sistema tem ${listaVeiculos.lenght} carros cadastrados`)
-
 const listaVeiculos = []
 let criaID = 0;
 let opcao = 1
 
 while (opcao != 6) {
-    opcao = Number(prompt(`Escolha uma das opções para interagir com o sistema:\n 1 - Cadastrar veículo\n 2 - Listar todos os veículos\n 3 - Filtrar veículos por marca\n 4 - Atualizar veículo\n 5 - Remover veículo\n 6 - Sair do sistema`))
+    opcao = Number(prompt(`Bem-vindo ao sistema de CRUD de veículos:\nNo momento, o sistema tem ${listaVeiculos.length} carro(s) cadastrado(s).\n Escolha uma das opções para interagir com o sistema:\n 1 - Cadastrar veículo\n 2 - Listar todos os veículos\n 3 - Filtrar veículos por marca\n 4 - Atualizar veículo\n 5 - Remover veículo\n 6 - Sair do sistema`))
 
     switch (opcao) {
         case 1:
@@ -23,7 +19,7 @@ while (opcao != 6) {
             atualizaVeiculo(listaVeiculos)
             break;
         case 5:
-            RemoverVeiculo()
+            removerVeiculo(listaVeiculos)
             break;
         case 6:
             alert("SAINDO...")
@@ -59,10 +55,9 @@ function cadastraVeiculo() {
         listaVeiculos.push(veiculo)
         alert("Cadastrado com sucesso")
     }
-
-
 }
 
+//Gera ids automáticos
 function geraId() {
     criaID++
     return criaID
@@ -70,39 +65,74 @@ function geraId() {
 
 
 function listarVeiculos(lista) {
-    lista.forEach((element) => console.log(`Lista de veiculo cadastrados:\nID: ${element.id}| Modelo: ${element.modelo}| Marca: ${element.marca}| Ano: ${element.ano}| Preço: ${element.preco}\n`))
+    //imprime veiculos 
+    let imprimeLista = "Lista de veiculos cadastrados:\n"
+
+    for (let i = 0; i < lista.length; i++) {
+        imprimeLista += (`ID: ${lista[i].id}| Modelo: ${lista[i].modelo}| Marca: ${lista[i].marca}| Ano: ${lista[i].ano}| Cor: ${lista[i].cor}| Preço: ${lista[i].preco}\n`)
+    }
+
+    alert(imprimeLista);
 }
 
 
 function filraVeiculo(lista) {
     let marca = prompt("Qual é a marca do veiculo?")
 
+    let imprimeLista = `Veiculos encontrados com a marca ${marca}\n`
+
+    //busca marca que for = a marca informada usando filter
     const carros = lista.filter((carro) => carro.marca == marca)
 
-    carros.forEach((element) => console.log(`ID: ${element.id}| Cor: ${element.cor}| Preço: R$${element.preco}`))
-
+    //imprime veiculos
+    if (carros.length == 0) {
+        alert("Marca não encontrada")
+    } else {
+        for (let i = 0; i < carros.length; i++) {
+            imprimeLista += (`ID: ${carros[i].id}|Modelo: ${carros[i].modelo}| Cor: ${carros[i].cor}| Preço: R$${carros[i].preco}\n`)
+        }
+        alert(imprimeLista)
+    }
 }
 
 
 function atualizaVeiculo(lista) {
     let identificador = Number(prompt("Qual é o identificador do veiculo?"))
 
-    const idSelecionado = lista.filter((pegaId) => pegaId.id == identificador)
-    if (idSelecionado.length == 0) {
+    //busca veiculo com o mesmo id usando filter metodo de filtar passando parâmentros
+    const carroSelecionado = lista.filter((carro) => carro.id == identificador)
+
+    //verifica se existe veiculos
+    if (carroSelecionado.length == 0) {
         alert("Veiculo não encontrado")
     } else {
-        let novaCor = prompt("Digite a nova cor.")
-        let novoPreco = Number(prompt("Digite o novo preço."))
-
-
-        // EM DESENVOLVIMENTO
-        
-        // const listaAtualizada = idSelecionado.forEach((element) => element.cor = novaCor, element.preco = novoPreco);
-        // lista.splice(idSelecionado - 1, 0, listaAtualizada)
+        for (let i = 0; i < lista.length; i++) {
+            if (lista[i].id == identificador) {
+                let novaCor = prompt("Digite a nova cor.")
+                let novoPreco = Number(prompt("Digite o novo preço."))
+                lista[i].cor = novaCor
+                lista[i].preco = novoPreco
+                alert("Alterado com sucesso!!")
+            }
+        }
     }
-    console.log(idSelecionado);
-
 }
 
+function removerVeiculo(lista) {
+    let identificador = Number(prompt("Qual é o identificador do veiculo?"))
 
-console.log(listaVeiculos);
+    const carroSelecionado = lista.filter((carro) => carro.id == identificador)
+    if (carroSelecionado.length == 0) {
+        alert("Veiculo não encontrado")
+    } else {
+        for (let i = 0; i < lista.length; i++) {
+            if (lista[i].id == identificador) {
+                let apagar = prompt("Deseja apagar? [S/N]")
+                if (apagar == "S" || apagar == "s") {
+                    lista.splice(lista[i], 1) //metodo de remoção
+                    alert("Removido com Sucesso!!")
+                }
+            }
+        }
+    }
+}
